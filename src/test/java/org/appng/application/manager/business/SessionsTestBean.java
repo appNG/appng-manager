5eab77bd19276169d4cd09fa21aa23b7413e269e
mm <mm@nbmm.aiticon.net>
2019-10-31 17:16:34 +0100

    MGR-74 fixed test

--- src/test/java/org/appng/application/manager/business/SessionsTestBean.java
+++ src/test/java/org/appng/application/manager/business/SessionsTestBean.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.appng.application.manager.business;
 
 import java.util.ArrayList;
@@ -27,10 +42,8 @@ public class SessionsTestBean extends Sessions {
 		SESSIONS.remove(session);
 		Session mocked = Mockito.mock(Session.class);
 		String id = session.getId();
-		if (!id.equals(currentSession)) {
-			Mockito.when(mocked.isExpired()).thenReturn(true);
-		}
 		Mockito.when(mocked.getId()).thenReturn(id);
+		Mockito.when(mocked.isExpired()).thenReturn(!id.equals(currentSession));
 		SESSIONS.add(mocked);
 	}
 
