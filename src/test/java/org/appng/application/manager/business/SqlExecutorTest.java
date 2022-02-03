8a324fd7b8c92a954179755c0326cce4b6a667bb
madness-inc <matthias.mueller@appng.org>
2022-02-03 14:38:34 +0100

    MGR-135

--- src/test/java/org/appng/application/manager/business/SqlExecutorTest.java
+++ src/test/java/org/appng/application/manager/business/SqlExecutorTest.java
@@ -16,6 +16,10 @@
 package org.appng.application.manager.business;
 
 import org.appng.api.support.CallableAction;
+import org.appng.core.domain.DatabaseConnection.DatabaseType;
+import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.flywaydb.core.internal.parser.Parser;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -32,6 +36,23 @@ public class SqlExecutorTest extends AbstractTest {
 
 		callableAction.perform();
 		validate(callableAction.getAction());
+	}
+
+	@Test
+	public void testParser() throws ReflectiveOperationException {
+		assertType("MySQLParser", "jdbc:mysql://localhost", DatabaseType.MYSQL);
+		assertType("MariaDBParser", "jdbc:mariadb://localhost", DatabaseType.MYSQL);
+		assertType("SQLServerParser", "jdbc:sqlserver://localhost", DatabaseType.MSSQL);
+		assertType("PostgreSQLParser", "jdbc:postgresql://localhost", DatabaseType.POSTGRESQL);
+		assertType("HSQLDBParser", "jdbc:hsqldb:hsql://localhost", DatabaseType.HSQL);
+	}
+
+	private void assertType(String className, String url, DatabaseType type) throws ReflectiveOperationException {
+		ClassicConfiguration cfg = new ClassicConfiguration();
+		cfg.setDataSource(url, null, null);
+		Parser parser = new SqlExecutor().getParser(type, cfg);
+		Assert.assertNotNull(parser);
+		Assert.assertEquals(className, parser.getClass().getSimpleName());
 	}
 
 }
